576039227a9c94d8a96bb10e197c24692496bb29
Diego Gonzalez <diegoaliriogm@ufps.edu.co>
2019-10-12 16:29:41 -0500

    probando logo

---
+++ resources/views/contenido/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="jquery-easing/jquery.easing.min.js"></script>
+  <script src="magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
